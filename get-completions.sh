4bf8df9424e746e352bea18d2c96328ee38f857f
Radka Janek <radka.janek@redhat.com>
2018-05-30 17:34:36 +0200

    gitlab PR #6

--- get-completions.sh
+++ get-completions.sh
@@ -19,7 +19,7 @@ get_completions()
     #echo "COMP_WORDS: '${COMP_WORDS[@]}'"
     COMP_LINE="$(local IFS=" "; echo "${COMP_WORDS[@]}")"
     #echo "COMP_LINE: '$COMP_LINE'"
-    COMP_CWORD=$(("${#COMP_WORDS[@]}" - 1)) # index into COMP_WORDS
+    COMP_CWORD=$((${#COMP_WORDS[@]} - 1)) # index into COMP_WORDS
     #echo "COMP_CWORD: '$COMP_CWORD'"
     COMP_POINT="${#COMP_LINE}" # index of cursor position
     #echo "COMP_POINT: '$COMP_POINT'"
