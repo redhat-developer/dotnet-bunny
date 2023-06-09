8dafdf8a7bf3f1720ac9e6d215efa55a8e5afe46
Tom Deseyn <tom.deseyn@gmail.com>
2023-06-09 19:48:35 +0200

    Fix orphaned grandchild processes from tests causing test runner to hang.
    These orphaned grandchildren cause WaitForExit to block because
    they keep standard output and standard error open.
    
    To fix this, we use the WaitForExitAsync method that was added in .NET 5.
    It enables reading the output until the CancellationToken is set.

--- Turkey/ProcessExtensions.cs
+++ Turkey/ProcessExtensions.cs
@@ -8,7 +8,7 @@ namespace Turkey
 {
     public static class ProcessExtensions
     {
-        public static Task WaitForExitAsync(this Process process, CancellationToken token, TextWriter standardOutput, TextWriter standardError)
+        public static async Task WaitForExitAsync(this Process process, CancellationToken token, TextWriter standardOutput, TextWriter standardError)
         {
             process.EnableRaisingEvents = true;
             var outputHandler = new DataReceivedEventHandler(
@@ -32,53 +32,20 @@ namespace Turkey
             process.ErrorDataReceived += errorHandler;
             process.BeginErrorReadLine();
 
-            var tcs = new TaskCompletionSource<bool>();
-            int completionOwner = 0;
-            CancellationTokenRegistration ctr = default;
-            ctr = token.Register(() => CompleteTask(cancel: true));
-            process.Exited += delegate { CompleteTask(cancel: false); };
-            if (process.HasExited) // In case the Process exited before we've added the handler.
+            try
             {
-                CompleteTask(cancel: false);
+                await process.WaitForExitAsync(token);
             }
-            return tcs.Task;
-
-            void CompleteTask(bool cancel)
+            catch (OperationCanceledException ex)
             {
-                // Only one caller can complete the task.
-                if (Interlocked.CompareExchange(ref completionOwner, 1, 0) == 1)
+                try
                 {
-                    return;
+                    process.Kill(entireProcessTree: true);
                 }
+                catch
+                { }
 
-                if (cancel)
-                {
-                    try
-                    {
-                        process.Kill();
-                    }
-                    catch // Before .NET 3, Kill can throw if the process has terminated in the meanwhile.
-                    { }
-
-                }
-                else
-                {
-                    process.WaitForExit();  // Wait until we've received all output.
-                    ctr.Dispose();          // Don't root objects via the CancellationToken.
-                }
-
-                process.OutputDataReceived -= outputHandler;
-                process.ErrorDataReceived -= errorHandler;
-
-                // We're done using the process, now it's safe to complete the Task.
-                if (cancel)
-                {
-                    tcs.SetCanceled();
-                }
-                else
-                {
-                    tcs.SetResult(true);
-                }
+                throw;
             }
         }
     }
